4395d36f203c7336ab393dac70291a14b1b6ed00
nnabighods <n.nabighods@gmail.com>
2022-02-18 00:20:40 -0700

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import {Button} from './ui/buttons'
+
 function App() {
  
   return (
     <>
-    <Button><div>blah</div><p>UI Button</p></Button>
-    <Button bgcolor='cornflowerblue'>UI Button</Button>
-    <Button bgcolor='orange' color='white'>UI Button</Button>
     </>
   );
 }
